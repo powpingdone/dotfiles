e2601ee53918e46b611bbd2f759ce61cf8ca79fb
Aidan Case <aidanzcase@gmail.com>
2022-08-13 12:47:24 -0500

    change some stuff

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ./.zshrc ~
+RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+./nvim/runme.sh
