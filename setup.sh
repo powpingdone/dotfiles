ed00b39aa3783a1d41f2fac5fc56be783b8b2e60
Aidan Case <aidanzcase@gmail.com>
2022-08-13 12:52:28 -0500

    p10k

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ln -s ./.zshrc ~
+ln -s ./.p10k.zsh ~
 RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 ./nvim/runme.sh
