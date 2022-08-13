d8984ff2e0f99c4943c1c11db122433b94c0a916
Aidan Case <aidanzcase@gmail.com>
2022-08-13 13:57:20 -0500

    remove old configs

--- nvim/runme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/.config/nvim/dein
-sh ./deininstall.sh ~/.config/nvim/dein
-cp *.vim *.json ~/.config/nvim/
-nvim -c ":CocInstall coc-clangd coc-rls coc-python|q"
--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-ln -s ./.zshrc ~
-ln -s ./.p10k.zsh ~
-RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-./nvim/runme.sh
+ln -s "$(realpath .zshrc)" ~
+ln -s "$(realpath .p10k.zsh)" ~
