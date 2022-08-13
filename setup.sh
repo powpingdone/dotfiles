9aa8a29bc7f5d63f1a9e69bc08a37d3aced6c273
Aidan Case <aidanzcase@gmail.com>
2022-08-13 16:02:19 -0500

    modules update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+git submodule init
+git submodule update --depth 1
+
+# nerd font
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo ""
+
 # zsh
 RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -8,4 +15,8 @@ ln -s "$(realpath .p10k.zsh)" ~
 
 # nvim 
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-nvim +PackerSync
+mkdir -p ~/.config/nvim/lua/user/
+ln -s "$(realpath init.lua)" ~/.config/nvim/lua/user/
+nvim  --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+echo -e "\nREMINDER:::\npackages needed: zsh zsh-autosuggestions zsh-syntax-highlighting\n\tneovim powerline-fonts fontawesome-fonts git-credential-libsecret"
