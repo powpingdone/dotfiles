24b30d3949633b2ad52289656e44c833ead07a6c
Aidan Case <aidanzcase@gmail.com>
2023-02-14 16:48:22 -0600

    update lazy

--- nvim/lua/plugins/plugs.lua
+++ nvim/lua/plugins/plugs.lua
@@ -7,4 +7,12 @@ return {
       colorscheme = "edge",
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufNewFile", "BufEnter" },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = { "BufNewFile", "BufEnter" },
+  },
 }
