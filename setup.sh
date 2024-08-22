93cff0587b411f854db8553c56b9de1f0b9c68dc
Aidan <aidanzcase@gmail.com>
2024-08-21 22:13:26 -0500

    emacs

--- setup.sh
+++ setup.sh
@@ -12,16 +12,11 @@ if [[ ! -x "$(which git)" ]]; then
 	exit 1
 fi
 
+mkdir -p ~/.config
+
+# update internal modules
 git submodule update --depth 1 --init --remote --merge
 set -e
-
-# nerd font
-if [[ -x "$(which fc-cache)" ]]; then
-	echo -e "\ncompiling fonts..."
-	mkdir -p ~/.local/share/fonts
-	nerd-fonts/install.sh
-	fc-cache -rf
-fi
 
 # zsh
 echo -e "\ninstalling omzsh and p10k"
@@ -32,32 +27,17 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 ln -fs "$(realpath .zshrc)" ~
 ln -fs "$(realpath .p10k.zsh)" ~
 
+
 # nvim
-mkdir -p ~/.config/
 echo -e "\nsymlinking neovim"
 ln -fs "$(realpath nvim)" ~/.config/
 
-# helix
-echo -e "\n symlinking helix"
-ln -fs "$(realpath helix)" ~/.config/
-
-# vscode
-#if [[ -x "$(which flatpak)" ]]; then
-#	echo -e "installing special vscode stuff"
-#	flatpak install -y com.visualstudio.code
-#	mkdir -p ~/.var/app/com.visualstudio.code/config/Code/User
-#	ln -fs "$(realpath settings.json)" ~/.var/app/com.visualstudio.code/config/Code/User/
-#	cat vscode_exts.txt | xargs -n 1 flatpak run com.visualstudio.code --install-extension
-#fi
-
-# toolbox vscode
-#mkdir -p ~/.local/bin
-#ln -fs "$(realpath toolbox-vscode/code.sh)" ~/.local/bin/code
+# emacs
+ln -fs "$(realpath doomemacs)" ~/.config/emacs
+ln -fs "$(realpath doomcfg)" ~/.config/doom
+~/.config/emacs/bin/doom install
 
 echo -e "\n\n:::REMINDERS:::"
 echo -e "packages needed:"
 echo -e "\tzsh zsh-autosuggestions zsh-syntax-highlighting neovim"
-echo -e "\tpowerline-fonts fontawesome-fonts git-credential-oauth"
-echo -e "\nmake sure you setup toolbox-vscode"
-echo -e "\nvscode also needs access to wayland"
-echo -e "vscode helper script is no longer installed"
+echo -e "\tgit-credential-oauth"
