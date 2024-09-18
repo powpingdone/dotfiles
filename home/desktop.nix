8c33ead7a146ff1925cb0fa5ca65dfa47b29b63a
Aidan <aidanzcase@gmail.com>
2024-09-17 19:07:05 -0500

    Add some select gnome settings

--- home/desktop.nix
+++ home/desktop.nix
@@ -6,6 +6,16 @@ lib.mkIf nixosConfig.ppd.desktop.enable {
   # enable dark mode for gtk4
   dconf = {
     enable = true;
-    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    settings = { 
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      "org/gnome/mutter".dynamic-workspaces = true;
+      "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = 
+        [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        binding = "<Super>Return";
+	command = "kgx";
+	name = "Open Terminal";
+      };
+    };
   };
 }
