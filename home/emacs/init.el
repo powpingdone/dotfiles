674aac22431cdf5bf3b49431cd0ad298ae4688db
Aidan <aidanzcase@gmail.com>
2024-09-23 19:13:16 -0500

    I don't know what is happening

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -1,6 +1,7 @@
 (defun ppd/reload-emacs ()
   (interactive)
   (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
-  (delete-file (expand-file-name "emacs.el" user-emacs-directory)))
+  (delete-file (expand-file-name "emacs.el" user-emacs-directory))
+  )
 
 (ppd/reload-emacs)
