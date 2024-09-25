a477b8f41b64fef820598f0c959bf4bec7c221d0
Aidan <aidanzcase@gmail.com>
2024-09-25 13:58:22 -0500

    Do not activate flatpak repo every time

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -54,13 +54,6 @@
 
     # flatpak
     services.flatpak.enable = true;
-    systemd.services.flatpak-repo = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.flatpak ];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
 
     # use ozone on desktop
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
