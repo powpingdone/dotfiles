c88624a9fbf57a4dd515c4ffe210226639d203d5
Aidan <aidanzcase@gmail.com>
2024-09-28 13:58:03 -0500

    fix emacs again

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -2,6 +2,7 @@
 
 (defun ppd/reload-emacs ()
   (interactive)
+  (delete-file (expand-file-name "emacs.el" user-emacs-directory))
   (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
   (delete-file (expand-file-name "emacs.el" user-emacs-directory))
   )
