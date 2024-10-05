6c4ec159bdde1f75522fc2618e4c31e521457edb
Aidan <aidanzcase@gmail.com>
2024-10-05 12:49:02 -0500

    Use spaces in org mode

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -7,6 +7,10 @@
     (error nil))
   (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
   (delete-file (expand-file-name "emacs.el" user-emacs-directory))
+  ; when reloading, this config part will reload org properly
+  (dolist (buf (buffer-list))
+   (with-current-buffer buf
+     (when (derived-mode-p 'org-mode) (org-reload))))
   )
 
 (ppd/reload-emacs)
