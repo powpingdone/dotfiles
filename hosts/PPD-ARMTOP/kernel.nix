941d2b493a0a8d9d382d11b53093c51cde3ba853
Aidan <aidanzcase@gmail.com>
2024-10-18 15:17:13 -0500

    Revert "change kernel"
    This reverts commit b3f87aca15d50d218c3333c972bf1538ec8d03d1.

--- hosts/PPD-ARMTOP/kernel.nix
+++ hosts/PPD-ARMTOP/kernel.nix
@@ -9,16 +9,19 @@ pkgs.linuxPackagesFor (pkgs.buildLinux {
   src = fetchFromGitHub {
     owner = "jhovold";
     repo = "linux";
-    rev = "wip/x1e80100-6.12-rc1";
-    hash = "sha256-PzE3Ittt787Lxl84Btz83AcE/LCN3IONhZKGZG34Qm8=";
+    rev = "wip/x1e80100-6.12-rc3";
+    hash = "sha256-kpuzjqcI4YGS+S9OvIUhm6z8xCGMA5h5+JlcHhoEETM=";
   };
-  version = "6.12.0-rc1";
+  version = "6.12.0-rc3";
   defconfig = "johan_defconfig";
 
   structuredExtraConfig = with lib.kernel; {
     MAGIC_SYSRQ = yes;
     EC_LENOVO_YOGA_SLIM7X = module;
   };
+  extraConfig = ''
+    DRM_PANIC_SCREEN_QR_CODE_URL ""
+  '';
 
   kernelPatches = [
     {
