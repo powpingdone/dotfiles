4bca52f16f6f7976c90d7156f01551e722daf668
Aidan <aidanzcase@gmail.com>
2024-11-02 21:02:53 -0500

    Change plugin

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -19,7 +19,7 @@
       pkgs.gst_all_1.gst-plugins-good
       pkgs.gst_all_1.gst-plugins-bad
       pkgs.gst_all_1.gst-plugins-ugly
-      pkgs.gst_all_1.gst-plugins-libav
+      pkgs.gst_all_1.gst-libav
     ];
 
     # enable debug info stuff
