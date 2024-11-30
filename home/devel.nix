5ef2b442cb8b1a8983fc675759d4616ecb0db161
Aidan <aidanzcase@gmail.com>
2024-11-29 22:25:46 -0600

    Add zsh init stuff for direnv

--- home/devel.nix
+++ home/devel.nix
@@ -2,6 +2,7 @@
   nixosConfig,
   pkgs,
   lib,
+  config,
   ...
 }: {
   config = lib.mkIf nixosConfig.ppd.devenv.enable {
@@ -9,5 +10,6 @@
     programs.direnv = {
       enable = true;
     };
+    programs.zsh.initExtra = ''eval "$(${config.programs.direnv.package}/bin/direnv hook zsh)"'';
   };
 }
