96a97dcf08dd1751c9b1b4d7ea8afea1a6a2ab70
Aidan <aidanzcase@gmail.com>
2025-01-23 14:11:47 -0600

    Remove pd-mapper

--- hosts/PPD-ARMTOP/default.nix
+++ hosts/PPD-ARMTOP/default.nix
@@ -14,18 +14,8 @@
 
   # this saves battery
   powerManagement.cpuFreqGovernor = "conservative";
-
-  # part of firmware
-  systemd.services.pd-mapper = {
-    enable = true;
-    description = "Qualcomm PD mapper service";
-    serviceConfig = {
-      ExecStart = "${pkgs.pd-mapper}/bin/pd-mapper";
-      Restart = "always";
-    };
-    wantedBy = ["multi-user.target"];
-  };
-
+  
+  # enable dtb
   hardware.deviceTree = {
     enable = true;
     name = "qcom/x1e80100-lenovo-yoga-slim7x.dtb";
