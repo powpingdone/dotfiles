2642f4ace7556b94ff68823f8d3bc0cf4c46b6d3
Aidan <aidanzcase@gmail.com>
2025-01-27 15:11:42 -0600

    Add zsh prompt

--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -120,5 +120,27 @@ in {
       nixd
       alejandra
     ];
+    
+    # since we're using vterm, add this to the zsh init
+    programs.zsh.initExtra = ''
+    vterm_printf() {
+        if [ -n "$TMUX" ] \
+            && { [ "$${TERM%%-*}" = "tmux" ] \
+                || [ "$${TERM%%-*}" = "screen" ]; }; then
+            # Tell tmux to pass the escape sequences through
+            printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+        elif [ "$${TERM%%-*}" = "screen" ]; then
+            # GNU screen (screen, screen-256color, screen-256color-bce)
+            printf "\eP\e]%s\007\e\\" "$1"
+        else
+            printf "\e]%s\e\\" "$1"
+        fi
+    }
+    vterm_prompt_end() {
+        vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+    }
+    setopt PROMPT_SUBST
+    PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
+    '';
   };
 }
