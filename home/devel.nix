1186e0d18304b8e904d8846e0527baf1ce474665
Aidan <aidanzcase@gmail.com>
2025-04-17 21:02:03 -0500

    Add gdbinit, deadlock detection

--- home/devel.nix
+++ home/devel.nix
@@ -5,10 +5,25 @@
   ...
 }: {
   config = lib.mkIf nixosConfig.ppd.devenv.enable {
-    home.packages = [pkgs.devenv];
+    home.packages = with pkgs; [
+      gdb
+      devenv
+    ];
+    
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
+    };
+
+    # gdb script to add cool thing for detecting deadlocks with `blocked` command
+    # taken from https://github.com/DamZiobro/gdb-automatic-deadlock-detector
+    xdg.configFile = {
+      "gdb/gdbinit".source =
+''
+python
+${lib.readFile ./gdbDisplayLockedThreads.py}
+end
+'';
     };
   };
 }
