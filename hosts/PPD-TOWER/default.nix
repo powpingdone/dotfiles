59bf99bb889d774c6230a94aa6f37819a9272dce
Aidan <aidanzcase@gmail.com>
2025-06-24 16:06:57 -0500

    Add overdrive

--- hosts/PPD-TOWER/default.nix
+++ hosts/PPD-TOWER/default.nix
@@ -13,6 +13,7 @@
   ];
 
   services.lact.enable = true;
+  hardware.amdgpu.overdrive.enable = true;
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "uas" "sd_mod"];
   boot.initrd.kernelModules = [];
