d0ac2b6c3865910ac8c8ccccf9b3b7358052a6e3
Aidan <aidanzcase@gmail.com>
2025-12-01 08:40:17 -0600

    Update Lockfile

--- home/emacs/early-init.el
+++ home/emacs/early-init.el
@@ -1,18 +1,24 @@
 (setopt package-enable-at-startup nil)
 
+;; ignore use-package :ensure
 (setopt use-package-ensure-function 'ignore)
 
+;; disable use-package package archives
 (setopt package-archives nil)
 
+;; gc optimization
 (setq gc-cons-threshold (* 80 1024 1024))
 
+;; on using native-comp
 (when (native-comp-available-p)
-(require 'xdg)
-(require 's)
-(setq native-comp-jit-compilation t)
-(startup-redirect-eln-cache
- (expand-file-name
-  (string-trim
-   (concat "emacs/eln-cache" (s-trim (shell-command-to-string "emacs -Q --fingerprint")) "/"))
-  (xdg-cache-home)))
+  (require 'xdg)
+  (require 's)
+  (setq native-comp-jit-compilation t)
+  
+  ;; set emacs cache to be based on the fingerprint of emacs itself
+  (startup-redirect-eln-cache
+   (expand-file-name
+    (string-trim
+     (concat "emacs/eln-cache" (s-trim (shell-command-to-string "emacs -Q --fingerprint")) "/"))
+    (xdg-cache-home)))
 )
