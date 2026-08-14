7cbf6dfeb38435c83cce525d38f63564c9d23cfe
Aidan <aidanzcase@gmail.com>
2026-08-14 16:33:26 -0500

    ghidra-switch-loader mess

--- modules/ghidra.nix
+++ modules/ghidra.nix
@@ -4,28 +4,26 @@
   pkgs,
   ...
 }: let
-  ghidra_pkg = pkgs.ghidra.withExtensions (gext:
-    with gext; [
-      machinelearning
-      ghidra-golanganalyzerextension
-      ret-sync
-      findcrypt
-      ghidra-switch-loader
-    ]);
 in {
   config = lib.mkIf config.ppd.ghidra.enable {
     environment.systemPackages = [
-      ghidra_pkg
+      (pkgs.ghidra.withExtensions (gext:
+          with gext; [
+            machinelearning
+            ghidra-golanganalyzerextension
+            ret-sync
+            findcrypt
+          ]
+        ++ [
+          pkgs.ghidra-switch-loader
+        ])
+      )
     ];
+
     nixpkgs.overlays = [
-                (final: prev: {
-                  ghidra-extensions = prev.ghidra-extensions.overrideScope (gefinal: geprev: {
-                    # I legit don't know why I need to pass the override over as well, but
-                    # nixpkgs tries callinv override after I override the scope which is *very bizzare*
-                    override = prev.ghidra-extensions.override;
-                    ghidra-switch-loader = prev.callPackage ./gsl/gsl.nix;
-                  });
-                })
+      (final: prev: {
+        ghidra-switch-loader = prev.callPackage ./gsl/gsl.nix {ghidra = prev.ghidra;};
+      })
     ];
   };
 }
